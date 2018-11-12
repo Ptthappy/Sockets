6db28f31e18be150e825ef6ef1209baa6ad3a212
Luis Petrella <aax_00ap@hotmail.com>
2018-11-12 10:29:40 -0400

    Bueno esto es casi lo que necesito 4now
    Todavía falta bb

--- src/com/lepg/client/ClientMain.java
+++ src/com/lepg/client/ClientMain.java
@@ -15,17 +15,20 @@ import java.util.Scanner;
 
 public class ClientMain {
     private static Socket socket = null;
-    private static Scanner s = new Scanner(System.in);
+    private static Scanner s = new Scanner(System.in); //Scanner para primitivos
+    private static Scanner s2 = new Scanner(System.in); //Scanner para el input
     private static ObjectInputStream in = null;
     private static ObjectOutputStream out = null;
+    private static String userInput = null;
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException  {
         System.out.println("1. Conectarse al servidor\n2. Salir");
         
         while (true) {
+            boolean connected = false;
             switch(s.nextInt()) {
             case 1:
-                connectToServer();
+                connected = connectToServer();
                 break;
                 
             case 2:
@@ -34,27 +37,50 @@ public class ClientMain {
             default:
                 System.err.println("Entráda inválida");
             }
-        }
-        /*
-        try {
-            System.out.println("Connecting");
-            socket = new Socket("localhost", 2000);
-            System.out.println("Connected");
-            System.out.println(socket.getInetAddress());
-            in = new ObjectInputStream(socket.getInputStream());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
+            if (connected)
+                break;
+        }  //Este loop hará la conexión con el servidor. Esto se hace para que no tire una excepción por si el servidor no está listo
+        
+        loop();  //Toda la transmisión está aquí
+        
+        out.writeObject("-1");  //Le avisa al servidor que el cliente se va a desconectar
+        Thread.sleep(1000);
+        System.exit(0);
     }
     
-    private static void connectToServer() {
+    private static boolean connectToServer() {
         try {
             System.out.println("Conectando");
             socket = new Socket("localhost", 2000);
             System.out.println("Conexión exitosa");
+            return true;
         } catch (Exception e) {
             e.printStackTrace();
+            return false;
         }
+    }
+    
+    private static void loop() {
+        while (true) {
+            System.out.println("Ingresar la dirección y el archivo a enviar. Ingrese una línea vacía para salir");
+            //Desktop/Test/TestFile.exe Por ejemplo
+            userInput = s2.nextLine();
+            if (userInput.equals(""))
+                break;
+            else {
+                try {
+                    speakToServer();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(0);
+                }
+            }
+        }
+    }
+    
+    private static void speakToServer() throws IOException {
+        out.writeObject(userInput);
+        System.out.println("Solicitud enviada");
     }
     
 }
--- src/com/lepg/server/ServerMain.java
+++ src/com/lepg/server/ServerMain.java
@@ -15,28 +15,48 @@ import java.net.ServerSocket;
 
 
 public class ServerMain {
-    
+    private static Socket socket = null;
+    private static ServerSocket ss = null;
+    private static ObjectInputStream in = null;
+    private static ObjectOutputStream out = null;
     
     public static void main(String[] args) throws IOException {
-        Socket socket = null;
-        ServerSocket ss = new ServerSocket(2000);
-        ObjectInputStream in = null;
-        ObjectOutputStream out = null;
+        ss = new ServerSocket(2000);
+        
         try {
             System.out.println("Esperando cliente");
-            socket = ss.accept();
+            socket = ss.accept();  //Recibe al cliente
             System.out.println("El cliente se ha conectado con la siguiente dirección: " + socket.getInetAddress());
             
             in = new ObjectInputStream( socket.getInputStream());
             out = new ObjectOutputStream( socket.getOutputStream());
             
-            
+            try {
+                loop();  //Loop donde se escucha al usuario hasta que este decide salir
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Error de transmisión");
+                e.printStackTrace();
+            } finally {
+                
+            }
             
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
     
+    private static void loop() throws IOException, ClassNotFoundException {
+        while (true) {
+            String userInput = (String)(in.readObject());
+            if (userInput.equals("-1"))
+                break;
+            else
+                listenClient();
+        }
+    }
     
+    private static void listenClient() {
+        
+    }
     
 }
